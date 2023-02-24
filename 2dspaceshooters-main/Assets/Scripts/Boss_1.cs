cd793d53d555fd28d2030be118570522efe0fc3b
softwareatlas <63039061+softwareatlas@users.noreply.github.com>
2023-02-24 17:08:25 +0300

    boss fixed

--- 2dspaceshooters-main/Assets/Scripts/Boss_1.cs
+++ 2dspaceshooters-main/Assets/Scripts/Boss_1.cs
@@ -73,27 +73,38 @@ IEnumerator BulletCoroutine() // bullet position
             yield return new WaitForSeconds(2f);
         }
         
-      } 
+      }
 
 
 
 
-// mermi çarpışma
-     void OnCollisionEnter2D(Collision2D other) 
+    // mermi çarpışma
+    void OnCollisionEnter2D(Collision2D other)
     {
 
         if (other.gameObject.CompareTag("PlayerBullet"))
         {
-            Debug.Log("Yarraaenemy");
-            health -= 1 ;
-           //Destroy(gameObject);
+
+            health -= 1;
+            //Destroy(gameObject);
         }
-         if (other.gameObject.CompareTag("Player"))
-         {
-            Debug.Log("Player'a carptik");
-             health -= 1 ;
-         }
-        
+        if (other.gameObject.CompareTag("DroneBullet"))
+        {
+
+            health -= 1;
+            //Destroy(gameObject);
+        }
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            health -= 1;
+        }
+        if (other.gameObject.CompareTag("Drone"))
+        {
+
+            health -= 1;
+        }
+
     }
     void bulletT()
     {
